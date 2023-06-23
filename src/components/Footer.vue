715dbf41996a7ee781e86cfd686c5a8e0a1e09d1
Bruce <yu86923@gmail.com>
2023-06-23 13:57:50 +0800

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <h2> This is Footer</h2>
+    </div>
+</template>
+
+<style>
+.wrapper{
+    width: 100%;
+    background-color: #2e5265;
+
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+    <div class="wrapper">
+        <h2> This is Header</h2>
+    </div>
+</template>
+
+<style>
+.wrapper{
+    width: 100%;
+    background-color: #2e5265;
+}
+</style>
